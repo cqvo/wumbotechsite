e4a10e054fc1b2d1b100c861713d5d9aba9b2b4a
Chris Vo <50410061+cqvo@users.noreply.github.com>
2025-11-18 11:33:14 -0500

    moved gtm to main route group layout

---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Sidebar } from '$lib/ui';
+
+	let { children } = $props();
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
+	<aside class="hidden md:block">
+		<Sidebar />
+	</aside>
+	<main>
+		{@render children()}
+	</main>
+</div>
