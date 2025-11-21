f09a90bb273c73ecd39f3261e8d966d6d1b20d9c
Chris Vo <50410061+cqvo@users.noreply.github.com>
2025-11-20 22:43:13 -0500

    add vercel analytics

--- src/lib/analytics/provider.svelte
+++ src/lib/analytics/provider.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectAnalytics();
 	injectSpeedInsights();
 </script>
 
