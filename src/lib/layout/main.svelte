b9b6d874fc908e5381094d9e81a1c851b4806462
Chris Vo <50410061+cqvo@users.noreply.github.com>
2025-12-01 14:37:30 -0500

    added mainlayout component

---
+++ src/lib/layout/main.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { NavMobileBar, NavRail, NavSidebar } from '$lib/ui';
+	let { children } = $props();
+</script>
+
+<div class="grid grid-cols-1 sm:grid-cols-[auto_1fr]">
+	<aside class="hidden sm:max-xl:block">
+		<NavRail />
+	</aside>
+
+	<aside class="hidden xl:block">
+		<NavSidebar />
+	</aside>
+	<main class="p-4 sm:px-8 md:px-16 lg:px-32 2xl:px-64">
+		{@render children()}
+	</main>
+
+	<footer class="sticky bottom-0 block sm:hidden">
+		<NavMobileBar />
+	</footer>
+</div>
--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -1,1 +1,3 @@
-export { default as Sidebar } from './sidebar.svelte';
+export { default as NavSidebar } from './nav-sidebar.svelte';
+export { default as NavMobileBar } from './nav-mobilebar.svelte';
+export { default as NavRail } from './nav-rail.svelte';
