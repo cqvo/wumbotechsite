4045b2c1286c04a45575a344cfb80e8ca5655778
Chris Vo <50410061+cqvo@users.noreply.github.com>
2025-12-04 14:24:23 -0500

    uploaded pfp to assets and added types to sidebar nav

--- src/lib/ui/nav-sidebar.svelte
+++ src/lib/ui/nav-sidebar.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-	import { CircleUserRoundIcon, HouseIcon, NewspaperIcon } from '@lucide/svelte';
+	import { CircleUserRoundIcon, GithubIcon, NewspaperIcon } from '@lucide/svelte';
+	import type { Icon } from '@lucide/svelte';
 	import { Avatar, Navigation } from '@skeletonlabs/skeleton-svelte';
+	import ProfilePic from '$lib/assets/trooper.jpg';
 
-	const linksSidebar = [
-		{ label: 'Home', href: '/', icon: HouseIcon },
+	interface NavItem {
+		label: string;
+		href: string;
+		icon: typeof Icon;
+	}
+
+	const linksSidebar: NavItem[] = [
 		{ label: 'About', href: '/about', icon: CircleUserRoundIcon },
-		{ label: 'Blog', href: '/blog', icon: NewspaperIcon }
+		{ label: 'Blog', href: '/blog', icon: NewspaperIcon },
+		{ label: 'Github', href: 'https://www.github.com/cqvo', icon: GithubIcon }
 	];
 
 	let anchorSidebar = 'btn hover:preset-tonal justify-start px-2 w-full';
@@ -17,13 +25,9 @@
 			{@render Me()}
 		</Navigation.Header>
 		<Navigation.Content>
-			<Navigation.Menu>
+			<Navigation.Menu class="mx-auto w-fit">
 				{#each linksSidebar as link (link)}
-					{@const Icon = link.icon}
-					<a href={link.href} class={anchorSidebar} title={link.label} aria-label={link.label}>
-						<Icon class="size-4" />
-						<span>{link.label}</span>
-					</a>
+					{@render NavItem(link)}
 				{/each}
 			</Navigation.Menu>
 		</Navigation.Content>
@@ -32,11 +36,20 @@
 
 {#snippet Me()}
 	<Avatar class="mx-auto size-36">
-		<Avatar.Image src="https://avatars.githubusercontent.com/u/50410061?v=4" />
+		<Avatar.Image src={ProfilePic} />
 		<Avatar.Fallback>CV</Avatar.Fallback>
 	</Avatar>
-	<div class="mx-auto">
+	<div class="mx-auto justify-items-center">
 		<h3 class="h3 font-mono">Chris Vo</h3>
 		<h6 class="text-surface-500 italic">Chief Wumbologist</h6>
+		<span class="font-bold">chris (at) wumbo.tech</span>
 	</div>
+{/snippet}
+
+{#snippet NavItem(link: NavItem)}
+	{@const Icon = link.icon}
+	<a href={link.href} class={anchorSidebar} title={link.label} aria-label={link.label}>
+		<Icon class="size-4" />
+		<span>{link.label}</span>
+	</a>
 {/snippet}
